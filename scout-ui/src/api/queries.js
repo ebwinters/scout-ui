81f44dce724daddfb4dbd0bff7f582b94293ebba
Ethan Winters <etwinter@microsoft.com>
2022-11-27 22:39:15 -0800

    use auth

--- scout-ui/src/api/queries.js
+++ scout-ui/src/api/queries.js
@@ -1,11 +1,10 @@
 export const getScouts = () => {
     let auth = process.env.REACT_APP_AUTH;
-    console.log("AUTH", auth);
     return fetch('https://scouts.azurewebsites.net/odata/Scouts', {
     method: 'GET',
         headers: {
             'Accept': 'application/json',
-            'Authorization': "Basic "
+            'Authorization': auth
         },
     })
     .then(response => response.json())
